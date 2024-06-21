3351efe337430d85c1b2fd93ebd52241e9afbab9
ladat <ladat01626362980@gmail.com>
2024-06-22 00:43:28 +0700

    BS-01 | search product by name

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,26 +1,52 @@
 import productService from "../services/productService";
 
 let getProduct = async (req, res) => {
-    let cateId = req.query.cateId;
-    if (cateId) {
-        let product = await productService.getProductService(cateId);
-        if (product) {
-            return res.status(200).json({
-                errCode: product.errCode,
-                message: product.message,
-                data: product.data,
-            })
-        } else {
-            return res.status(404).json({
-                errCode: 404,
-                message: "not found",
-            })
+    try {
+        let params = "";
+        for (let key in req.query)
+            params = key;
+        console.log(params);
+        if (params && params == "cateId") {
+            let cateId = req.query.cateId;
+            if (cateId) {
+                let product = await productService.getProductService(cateId);
+                if (product) {
+                    return res.status(200).json({
+                        errCode: product.errCode,
+                        message: product.message,
+                        data: product.data,
+                    })
+                } else {
+                    return res.status(404).json({
+                        errCode: 404,
+                        message: "not found",
+                    })
+                }
+            }
+        } else if (params && params == "search") {
+            let keyWord = req.query.search;
+            if (keyWord) {
+                let product = await productService.getSearchProductService(keyWord);
+                if (product) {
+                    return res.status(200).json({
+                        errCode: product.errCode,
+                        message: product.message,
+                        data: product.data,
+                    })
+                } else {
+                    return res.status(404).json({
+                        errCode: 404,
+                        message: "not found",
+                    })
+                }
+            }
         }
 
-    } else {
-        return res.status(404).json({
-            errCode: 404,
-            message: "not found",
+    } catch (e) {
+        console.log("Lỗi: ", e);
+        return res.status(500).json({
+            errCode: 500,
+            message: "Error from server",
         })
     }
 }
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,4 +1,4 @@
-import { where } from "sequelize";
+import { Op } from "sequelize";
 import db from "../models/index";
 let getProductService = async (cateId) => {
     return new Promise(async (resolve, reject) => {
@@ -8,7 +8,6 @@ let getProductService = async (cateId) => {
                     cateId: cateId,
                 }
             })
-            console.log("Check product: ", product);
             if (product && product.length > 0) {
                 resolve({
                     errCode: 0,
@@ -23,7 +22,6 @@ let getProductService = async (cateId) => {
                 })
             }
         } catch (e) {
-            console.log("Lỗi: ", e);
             reject({
                 errCode: -1,
                 message: "Get product failed",
@@ -32,7 +30,37 @@ let getProductService = async (cateId) => {
         }
     })
 }
+let getSearchProductService = async (keyWord) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let product = await db.Product.findAll({
+                where: {
+                    productName: {
+                        [Op.like]: `%${keyWord}%`,
+                    },
+                }
+            })
+            if (product && product.length > 0) {
+                console.log("Product : ", product)
+                resolve({
+                    errCode: 0,
+                    message: "OK",
+                    data: product,
+                })
+            } else {
+                resolve({
+                    errCode: 1,
+                    message: "Not fault data",
+                    data: null,
+                })
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 
 module.exports = {
-    getProductService: getProductService
+    getProductService: getProductService,
+    getSearchProductService: getSearchProductService,
 }
